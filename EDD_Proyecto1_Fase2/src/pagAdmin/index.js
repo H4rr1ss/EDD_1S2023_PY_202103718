dc8a316960e20aeeb79ee639a20c82d98986564d
harry <harry7801234@gmail.com>
2023-03-26 19:45:04 -0600

    [progress]: pagina admin

---
+++ EDD_Proyecto1_Fase2/src/pagAdmin/index.js
@@ -0,0 +1,50 @@
+const dropArea = document.querySelector(".drag-area");
+const dragText = dropArea.querySelector("h2");
+const button = dropArea.querySelector("button");
+const input = dropArea.querySelector("#input-file");
+let files;
+
+button.addEventListener("click", (e) => {
+    input.click();
+}); 
+
+input.addEventListener("change", (e) => {
+    files = this.files;
+    dropArea.classList.add("active");
+    showFiles(files);
+    dropArea.classList.remove("active");
+});
+
+
+dropArea.addEventListener("dragover", (e) =>{
+    e.preventDefault();
+    dropArea.classList.add("active");
+    dragText.textContent = "Suelte para cargar los archivos";
+});
+
+dropArea.addEventListener("dragleave", (e) =>{
+    e.preventDefault();
+    dropArea.classList.remove("active");
+    dragText.textContent = "Arrastre y suelte los archivos aquí para subirlos";
+});
+
+dropArea.addEventListener("drop", (e) =>{
+    e.preventDefault();
+    dropArea.classList.remove("active"); 
+    dragText.textContent = "Arrastre y suelte los archivos aquí para subirlos";
+});
+
+
+function showFiles(files){
+    if(files.length == undefined){
+        processFiles(files);
+    }else{
+        for(const file of files){
+            processFiles(file);
+        }
+    }
+}
+
+function processFiles(file){
+
+}
